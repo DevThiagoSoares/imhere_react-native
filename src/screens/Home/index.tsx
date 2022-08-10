1e65cfe67c79a4965dbc59afb13bdc10b942d669
Thiago Soares <thiago.soares@grupoicts.com.br>
2022-08-10 16:19:04 -0400

    init

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,37 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+
+import { Participant } from '../../components/Participant';
+
+export function Home() {
+
+    function handleParticipantAdd() {
+        console.log("Você clicou")
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}>
+                Nome de Evento
+            </Text>
+
+            <Text style={styles.eventDate}>
+                Sexta, 4 de Novembro de 2022
+            </Text>
+
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome"
+                    placeholderTextColor="#6b6b6b"
+                />
+                
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
